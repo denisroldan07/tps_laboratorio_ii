8f4f92bad3c7d43565e8795e35e19ee97f0efa4e
Denis Roldan <denisroldansl@gmail.com>
2022-05-01 20:40:23 -0300

    Trabajo practico 2 version final

---
+++ TP2/Entidades/Ciclomotor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color): base(chasis,marca,color)
+        {
+           
+        }
+
+        public override ETamanio Tamanio => ETamanio.Chico;
+
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>  
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
